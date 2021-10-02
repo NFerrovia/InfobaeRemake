2f6c7b310799d6fcf2209412cdce79a5061ec5d9
NFerrovia <nicolasferrovia@gmail.com>
2021-10-02 16:23:50 -0300

    mergefix y alert alfinal de la pagina

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 window.onscroll = function (ev) {
   if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
-    console.log('hola'); // you're at the bottom of the page
+    alert('llegaste alfinal de la pagina!');
   }
 };
